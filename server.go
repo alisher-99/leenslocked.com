ec177e7a038da5733b6384b67ee10306e3ea547f
alisher-99 <you@example.com>
2022-06-08 20:16:55 +0600

    404 Not Found

--- server.go
+++ server.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my bad page!<h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch with <a href=\"mailto:alisher1999@gmail.com\">alisher1999@gmail.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>404 not FOUND:(<h1><p>Please email us</p>")
 	}
 }
 
