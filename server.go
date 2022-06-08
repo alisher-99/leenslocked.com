97e85ea5cf8303e29a46d8b9b0bf6ff6527669fd
alisher-99 <you@example.com>
2022-06-08 16:44:20 +0600

    Second Version

--- server.go
+++ server.go
@@ -6,7 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my page!<h1>")
+	fmt.Println("TaimA$$")
+	fmt.Fprint(w, "<h1>Welcome to my bad page!<h1>")
 }
 
 func main() {
